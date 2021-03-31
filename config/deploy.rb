6d7d06d663df4ef1a5e43be30196b8c469206c7e
Kate Lynch <katelynch@princeton.edu>
2021-03-31 15:56:17 -0400

    WIP Capistrano deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,6 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-OJS_CONFIG = YAML.load(File.read(File.expand_path('../ojs_config.yml', __FILE__)))["#{fetch(:stage)}"].symbolize_keys!
 set :application, "openpublishing"
 set :repo_url, "git@github.com:pulibrary/openpublishing.git"
 
@@ -10,11 +9,44 @@ set :repo_url, "git@github.com:pulibrary/openpublishing.git"
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/openpublishing"
+set :shared_path, "/var/local"
 
 set :user, "deploy"
 
 namespace :ojs do
 
-  set :ojs_version_no, OJS_CONFIG[:ojs_version]
+  set :ojs_root, "#{fetch(:deploy_to)}/ojs"
+  set :ojs_file_uploads, File.join(fetch(:shared_path), 'files')
+
+  desc "Create shared files directory"
+  task :prepare_shared_paths do
+    on release_roles :app do
+      unless test("[ -d #{File.join(fetch(:ojs_file_uploads))} ]")
+        execute :mkdir, fetch(:ojs_file_uploads)
+      end
+      execute :mkdir, '-p', fetch(:ojs_file_uploads)
+      execute :chmod,  " -R 775 #{fetch(:ojs_file_uploads)}"
+
+      info "Created file uploads directory link"
+    end
+  end
+
+  desc "Link shared OJS files"
+  task :link_config do
+    on roles(:app) do |host|
+      execute "cd #{release_path}/ojs && ln -sf #{fetch(:shared_path)}/config.inc.php config.inc.php"
+      info "linked config inc PHP file to current release"
+    end
+  end
+
+end
+
+namespace :deploy do
+
+  desc "Set file system directories and linked files"
+  task :after_deploy_check do
+    invoke "ojs:prepare_shared_paths"
+    invoke "ojs:link_config"
+  end
 
 end
